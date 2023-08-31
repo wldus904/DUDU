d4acb856d373f5eae1451e36fead634883f8d28b
wldus904 <jypark2781@gamil.com>
2023-08-31 14:51:47 +0900

    feat: radio, radio group component 추가

---
+++ src/components/input/RadioGroup.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import Radio from "./Radio";
+
+const RadioGroupWrapper = styled.fieldset`
+    display: flex;
+    flex-direction: ${(props) => (props.topTitle ? "column" : "row")};
+    align-items: center;
+    border: none;
+`;
+const Title = styled.span`
+    font-weight: 600p;
+    margin-bottom: 3px;
+    margin-right: 10px;
+`;
+
+const RadioGroupBox = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+const RadioGroup = ({ setValue, radios, topTitle, leftTitle, groupName, disabled }) => {
+    const onChangeValue = (e) => {
+        setValue(e.target.value);
+    };
+
+    return (
+        <RadioGroupWrapper topTitle={topTitle} disabled={disabled}>
+            <Title>{topTitle ?? leftTitle}</Title>
+            <RadioGroupBox onChange={onChangeValue}>
+                {radios.map((radio) => {
+                    return (
+                        <Radio
+                            key={radio.value}
+                            value={radio.value}
+                            defaultChecked={radio.defaultChecked}
+                            name={groupName}
+                        >
+                            {radio.label}
+                        </Radio>
+                    );
+                })}
+            </RadioGroupBox>
+        </RadioGroupWrapper>
+    );
+};
+
+export default RadioGroup;
