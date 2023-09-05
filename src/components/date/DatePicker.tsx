8349abab779da94b83f18855dbb6725bda220921
wldus904 <jypark2781@gamil.com>
2023-09-05 13:21:57 +0900

    design: date picker style 수정

--- src/components/date/DatePicker.tsx
+++ src/components/date/DatePicker.tsx
@@ -28,24 +28,31 @@ const CalendarWrapper = styled.div`
     width: 0;
     height: 0;
     transition-property: width, height;
-    transition-duration: 0s, 0.5s;
+    transition-duration: 0s, 0.2s;
     transition-delay: 0.6s, 0s;
 
     &.on {
         width: 240px;
         height: 220px;
         transition-property: width, height;
-        transition-duration: 0s, 0.5s;
+        transition-duration: 0s, 0.2s;
         transition-delay: 0s, 0.1s;
     }
 `;
 
 const CalendarBox = styled.div`
+    display: none;
     position: absolute;
     width: 100%;
     height: 100%;
     top: 10px;
     right: 0;
+
+    &.on {
+        display: block;
+        transition-property: display;
+        transition-delay: 0.2s;
+    }
 `;
 
 const DatePicker = (props) => {
@@ -67,7 +74,7 @@ const DatePicker = (props) => {
             </DatePickerBox>
             <CalendarWrapper className={isShowCalendar ? "on" : ""}>
                 {/* {isShowCalendar && ( */}
-                <CalendarBox>
+                <CalendarBox className={isShowCalendar ? "on" : ""}>
                     <Calendar currentDate={currentDate} setValue={setValue}></Calendar>
                 </CalendarBox>
                 {/* )} */}
