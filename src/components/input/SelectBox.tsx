f7159bdcf52ecad022dc8562970dcc56a14692a1
wldus904 <jypark2781@gamil.com>
2023-09-05 13:21:21 +0900

    feat: select box component 수정

--- src/components/input/SelectBox.tsx
+++ src/components/input/SelectBox.tsx
@@ -1,7 +1,25 @@
 import styled from "styled-components";
 
-const SelectBox = () => {
-    return <div></div>;
+const BasicSelectBox = styled.select`
+    border: 1px solid #d6dbe4;
+`;
+
+const Option = styled.option`
+    font-size: 12px;
+`;
+
+const SelectBox = ({ options, ...rest }) => {
+    return (
+        <BasicSelectBox {...rest}>
+            {options.map((option) => {
+                return (
+                    <Option value={option.value ?? option} key={option.value ?? option}>
+                        {option.title ?? option}
+                    </Option>
+                );
+            })}
+        </BasicSelectBox>
+    );
 };
 
 export default SelectBox;
