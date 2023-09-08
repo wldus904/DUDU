5037babcab892e2aa80d87efb2d818325972a776
wldus904 <jypark2781@gamil.com>
2023-09-08 16:42:10 +0900

    design: date picker style 수정

--- src/components/input/SelectBox.tsx
+++ src/components/input/SelectBox.tsx
@@ -2,19 +2,24 @@ import styled from "styled-components";
 
 const BasicSelectBox = styled.select`
     height: 32px;
-    min-width: ${(props) => props.width ?? "70px"};
+    min-width: ${(props) => (props.width ? props.width : props.outline ? "70px" : "0")};
     border-radius: 4px;
-    border: 1px solid #d6dbe4;
-    padding: 0 10px 0 5px;
+    border: ${(props) => (props.outline ? "1px solid #d6dbe4" : "none")};
+    padding: ${(props) => (props.outline ? "0 10px 0 5px" : "0")};
     cursor: pointer;
     -webkit-appearance: none;
     -moz-appearance: none;
     appearance: none;
     text-align: center;
+
+    &:focus {
+        outline: none;
+    }
 `;
 
 const Option = styled.option`
     font-size: 14px;
+    width: 100px !important;
 `;
 
 const SelectBox = ({ options, ...rest }) => {
