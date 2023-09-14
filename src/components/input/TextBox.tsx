6efc9fcfe3ac959a30ead03ac574b0da6bb6398b
wldus904 <jypark2781@gamil.com>
2023-09-14 15:12:21 +0900

    feat: text box component validation 기능 추가

--- src/components/input/TextBox.tsx
+++ src/components/input/TextBox.tsx
@@ -1,13 +1,31 @@
 import styled from "styled-components";
 import { theme } from "@/styles/theme";
+import { useState } from "react";
+
+const TextBoxWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    border-radius: 4px;
+    border: 1px solid #d6dbe4;
+    padding: 0 5px;
+    transition: all ease 0.2s;
+
+    &:focus-within {
+        border: 1px solid ${theme.colors.warning};
+        box-shadow: 0 0 2px ${theme.colors.warning};
+    }
+
+    &.invalid {
+        border: 1px solid ${theme.colors.error};
+        box-shadow: 0 0 2px ${theme.colors.error};
+    }
+`;
 
 const BasicTextBox = styled.input`
     height: 32px;
     width: ${(props) => props.width ?? "100%"};
     font-size: 14px;
-    border-radius: 4px;
-    border: 1px solid #d6dbe4;
-    transition: all ease 0.1s;
+    border: none;
 
     &::placeholder {
         color: #d5d5d5;
@@ -15,16 +33,34 @@ const BasicTextBox = styled.input`
 
     &:focus {
         outline: none;
-        border: 1px solid ${theme.colors.warning};
-        box-shadow: 0 0 2px ${theme.colors.warning};
     }
 `;
 
-const TextBox = ({ children, placeholder, type, ...rest }) => {
+const TextBox = ({ children, placeholder, rules, ...rest }) => {
+    const [isValid, setIsValid] = useState(true);
+
+    const checkValid = (value) => {
+        let res = true;
+
+        rules.forEach((validator) => {
+            if (!validator(value)) res = false;
+        });
+
+        setIsValid(res);
+        rest.setValid(isValid);
+    };
+
     return (
-        <BasicTextBox placeholder={placeholder} {...rest} type={type ?? "text"}>
-            {children}
-        </BasicTextBox>
+        <TextBoxWrapper className={isValid ? "" : "invalid"}>
+            <BasicTextBox
+                onBlur={(e) => checkValid(e.target.value)}
+                placeholder={placeholder}
+                {...rest}
+                type="text"
+            >
+                {children}
+            </BasicTextBox>
+        </TextBoxWrapper>
     );
 };
 
