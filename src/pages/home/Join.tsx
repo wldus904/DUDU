8eab4be513ba9c35aed6d456271310e375f3122b
wldus904 <jypark2781@gamil.com>
2023-09-14 15:13:00 +0900

    feat: 회원 가입 component 수정

--- src/pages/home/Join.tsx
+++ src/pages/home/Join.tsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import TextBox from "@/components/input/TextBox";
+import Password from "@/components/input/Password";
 import MainIcon from "@/components/icon/MainIcon";
 import RadioGroup from "@/components/input/RadioGroup";
 import DatePicker from "@/components/date/DatePicker";
+import Button from "@/components/button/Button";
+import { required, phoneNumberValidator, emailValidator, passwordValidator } from "@/service/valid";
 
 const InputWrapper = styled.div`
     display: flex;
@@ -20,36 +23,58 @@ const MainIconBox = styled.div`
     margin-bottom: 20px;
 `;
 
-const InputBox = styled.div`
+const InputBox = styled.form`
     max-width: 400px;
+    min-width: 300px;
     padding: 20px;
     background-color: #fff;
     box-shadow: 0 0 4px #d6dbe4;
-
-    input[type="text"] {
-        margin-bottom: 5px;
-    }
 `;
 
-const RadioGroupBox = styled.div`
-    margin-top: 10px;
+const OptionBox = styled.div`
+    ${(props) => (props.flex ? "display: flex; align-items: center;" : "")}
+    margin-bottom: 5px;
 `;
 
-const DatePickerBox = styled.div`
-    margin-top: 10px;
+const Title = styled.div`
+    min-width: 70px;
 `;
 
-// 1. 입력 항목: 이메일, 비밀번호, 이름, 성별, 생일
 const Join = () => {
-    const [id, setId] = useState("");
+    const [email, setEmail] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
     const [pwd, setPwd] = useState("");
+    const [checkPwd, setCheckPwd] = useState("");
     const [name, setName] = useState("");
     const [gender, setGender] = useState("M");
     const [birthDay, setBirthDay] = useState("");
+    const [isValid, setIsValid] = useState(false);
+    const [loading, setLoading] = useState(false);
     const radios = [
         { label: "남자", value: "M", defaultChecked: true },
         { label: "여자", value: "W" },
     ];
+
+    useEffect(() => {
+        return console.log("isValid ::: ", isValid);
+    }, [isValid]);
+
+    const startJoin = () => {
+        setLoading(true);
+        setMsg("");
+
+        if (email === "test" && pwd === "test") {
+            localStorage.setItem("user", "abcd1234");
+            router.push("/main/dashboard");
+        } else {
+            setMsg("아이디 또는 비밀번호를 잘못 입력했습니다");
+            setLoading(false);
+        }
+    };
+
+    const checkPwdValidator = () => {
+        return pwd === checkPwd;
+    };
 
     return (
         <InputWrapper>
@@ -57,41 +82,87 @@ const Join = () => {
                 <MainIconBox>
                     <MainIcon />
                 </MainIconBox>
-                <TextBox
-                    placeholder="이메일"
-                    value={id}
-                    width="98%"
-                    onChange={(e) => setId(e.target.value)}
-                />
-                <TextBox
-                    placeholder="비밀번호"
-                    value={pwd}
-                    width="98%"
-                    onChange={(e) => setPwd(e.target.value)}
-                />
-                <TextBox
-                    placeholder="이름"
-                    value={name}
-                    width="98%"
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <RadioGroupBox>
+                <OptionBox>
+                    <TextBox
+                        placeholder="이메일"
+                        rules={[required, emailValidator]}
+                        value={email}
+                        width="98%"
+                        onChange={(e) => setEmail(e.target.value)}
+                        setValid={setIsValid}
+                        autoComplete="off"
+                    />
+                </OptionBox>
+                <OptionBox>
+                    <TextBox
+                        placeholder="전화번호"
+                        rules={[required, phoneNumberValidator]}
+                        value={phoneNumber}
+                        width="98%"
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                        setValid={setIsValid}
+                        autoComplete="off"
+                    />
+                </OptionBox>
+                <OptionBox>
+                    <Password
+                        placeholder="비밀번호"
+                        rules={[required, passwordValidator]}
+                        value={pwd}
+                        width="98%"
+                        onChange={(e) => setPwd(e.target.value)}
+                        setValid={setIsValid}
+                    />
+                </OptionBox>
+                <OptionBox>
+                    <Password
+                        placeholder="비밀번호 확인"
+                        rules={[required, passwordValidator, checkPwdValidator]}
+                        value={checkPwd}
+                        width="98%"
+                        onChange={(e) => setCheckPwd(e.target.value)}
+                        setValid={setIsValid}
+                    />
+                </OptionBox>
+                <OptionBox>
+                    <TextBox
+                        placeholder="이름"
+                        rules={[required]}
+                        value={name}
+                        width="98%"
+                        onChange={(e) => setName(e.target.value)}
+                        setValid={setIsValid}
+                        autoComplete="off"
+                    />
+                </OptionBox>
+                <OptionBox flex>
+                    <Title>성별</Title>
                     <RadioGroup
                         radios={radios}
-                        leftTitle="성별"
+                        rules={[required]}
                         groupName="gender"
                         setValue={(value) => setGender(value)}
+                        setValid={setIsValid}
                     />
-                </RadioGroupBox>
-                <DatePickerBox>
+                </OptionBox>
+                <OptionBox flex>
+                    <Title>생년월일</Title>
                     <DatePicker
                         setValue={setBirthDay}
-                        placeholder="생년월일"
+                        rules={[required]}
+                        placeholder="YYYY-MM-DD"
                         aligns="left top"
                         className="datePicker"
+                        setValid={setIsValid}
                     />
-                </DatePickerBox>
+                </OptionBox>
             </InputBox>
+
+            {isValid ?? (
+                <Button color="error" loading={loading} onClick={startJoin}>
+                    회원가입
+                </Button>
+            )}
         </InputWrapper>
     );
 };
