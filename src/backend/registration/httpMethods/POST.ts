0477a2eef39995dcb40a91487074fe0768f1155a
wldus904 <jypark2781@gamil.com>
2023-09-26 17:24:15 +0900

    feat: 회원 가입 post

--- src/backend/registration/httpMethods/POST.ts
+++ src/backend/registration/httpMethods/POST.ts
@@ -1,18 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { readAndWriteFile } from "@/utils/file";
 
 export function POST(req: NextApiRequest, res: NextApiResponse) {
-    const path = require("path");
-    const fs = require("fs");
-    fs.writeFile("test.json", req.body);
-
-    // const users = localStorage.getItem("USERS");
-    // console.log("users before ::: ", users);
-    // users = users ? JSON.parse(localStorage.getItem("USERS")) : {};
-    // console.log("users ::: ", users);
-    // const data = JSON.parse(req.body);
-    // console.log("data ::: ", data);
-    // users[data.email] = data;
-    // console.log("users input ::: ", users);
-    // localStorage.setItem("USERS", users);
+    const data = JSON.parse(req.body);
+    // api 대신
+    readAndWriteFile("user.json", (file) => {
+        return { ...file, [data.email]: data } ?? { [data.email]: data };
+    });
     res.status(200).json({ res: "success" });
 }
